ae989e4a158b4684ee87a1cd0a1f93f831eddd9f
Maxwell Wang <maxwang@udel.edu>
2022-05-17 10:05:43 -0400

    learned how to sqrt in c with formatting

---
+++ src/07-compile/sqrtC.c
@@ -0,0 +1,16 @@
+/*
+    The Hello World Program in C
+ */
+
+/* C math and I/O libraries */
+#include <math.h>
+#include <stdio.h>
+
+/* main function definition: */
+int main() {
+    // statements end in a semicolon (;)
+    printf("Hello World\n");
+    printf("sqrt(4) is %f\n", sqrt(4));
+
+    return 0;  // main returns value 0
+}
