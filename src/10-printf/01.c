a7a485e9b0ac2ddbcf29575564b2d119cb6c9807
mwang840 <maxwang@udel.edu>
2022-05-18 11:41:02 -0400

    attempted but got stuck on printf formatting

---
+++ src/10-printf/01.c
@@ -0,0 +1,15 @@
+/* edit this file so the program will display
+ * "First is 3 and second is 7.253."
+ * but with the actual values of "first" and "second" 
+ */
+
+#include <stdio.h> // necessary for printf
+#include <math.h>
+int main () {
+	int first = 9;
+	float second = 3.159;
+	printf("%d\n", first/3, second);
+
+	return 0;
+}
+// Nothing happening here!
