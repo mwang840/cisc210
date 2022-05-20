b06ecda0ea5a956f32a4649df16e4db952900c8a
mwang840 <maxwang@udel.edu>
2022-05-19 20:39:30 -0400

    deleted 11.c file and worked on 13.c file

---
+++ src/10-printf/13.c
@@ -0,0 +1,29 @@
+/* 
+ * This program should print this tree:
+ 
+   *
+  ***
+ *****
+*******
+  ***
+   
+  * And immediately below the tree, print this cat:
+
+/\   /\
+  o o
+ = * =
+ \___/
+ 
+ */
+
+#include <stdio.h>
+
+int main(void) {
+    char stars = '*';
+    printf("\t%c\n", stars);
+    printf("   %c\n", stars);
+    
+   /* Type your code here. */
+
+   return 0;
+}
